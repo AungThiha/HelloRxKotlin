f55d13a0a53f8be12eb90e12cf1742134eb6bdf9
Aung Thiha <mr.aungthiha@gmail.com>
2018-12-05 22:57:36 +0700

    move to appropriate package

--- src/main/kotlin/starting/four.kt
+++ src/main/kotlin/starting/four.kt
@@ -1,8 +1,22 @@
+package starting
+
+import episodeI
+import episodeII
+import episodeIII
+import episodeIV
+import episodeIX
+import episodeV
+import episodeVI
+import episodeVII
+import episodeVIII
+import rogueOne
+import solo
+import exampleOf
 import io.reactivex.Observable
 import io.reactivex.rxkotlin.toObservable
 
 fun main(args: Array<String>){
-    exampleOf("creating observables"){
+    exampleOf("creating observables") {
         val mostPopular = Observable.just(episodeV)
         val originalTrilogy = Observable.just(episodeIV, episodeV, episodeVI)
         val prequelTrilogy = Observable.just(listOf(episodeI, episodeII, episodeIII))
