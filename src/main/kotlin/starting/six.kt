210c8e06a84f1b800ca7bc9393abd080535f07b3
Aung Thiha <mr.aungthiha@gmail.com>
2018-12-10 17:20:36 +0700

    example usage of Single

--- src/main/kotlin/starting/six.kt
+++ src/main/kotlin/starting/six.kt
@@ -2,8 +2,11 @@ package starting
 
 import exampleOf
 import io.reactivex.Observable
+import io.reactivex.Single
 import io.reactivex.disposables.CompositeDisposable
 import io.reactivex.rxkotlin.subscribeBy
+import java.io.File
+import kotlin.text.Charsets.UTF_8
 
 sealed class Droid : Throwable() {
     class OU812 : Droid()
@@ -14,11 +17,12 @@ sealed class FileReadError : Throwable() {
 }
 
 fun main(args: Array<String>) {
+
     exampleOf("create") {
 
         val subscriptions = CompositeDisposable()
 
-        val droids = Observable.create<String> {emitter ->  
+        val droids = Observable.create<String> { emitter ->
             emitter.onNext("R2-D2")
             emitter.onError(Droid.OU812())
             emitter.onNext("C-3PO")
@@ -34,4 +38,34 @@ fun main(args: Array<String>) {
         subscriptions.add(observer)
 
     }
+
+    exampleOf("Single") {
+
+        val subscriptions = CompositeDisposable()
+
+        fun loadText(filename: String): Single<String> {
+            return Single.create { emitter ->
+                val file = File(filename)
+
+                if (!file.exists()){
+                    return@create emitter.onError(FileReadError.FileNotFound())
+                }
+
+                val contents = file.readText(UTF_8)
+
+                return@create emitter.onSuccess(contents)
+            }
+        }
+
+        val observer = loadText("ANewHope.txt")
+                .subscribe({
+                    println(it)
+                }, {
+                    println("Error, $it")
+                })
+
+        subscriptions.add(observer)
+
+    }
+
 }
