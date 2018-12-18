7506d62d1d32f5680936ce4c546c7bdfd9729a25
Aung Thiha <mr.aungthiha@gmail.com>
2018-12-18 22:48:17 +0700

    publishsubject

--- src/main/kotlin/SupportCode.kt
+++ src/main/kotlin/SupportCode.kt
@@ -15,3 +15,23 @@ const val episodeVI = "Return of the Jedi"
 const val episodeVII = "The Force Awakens"
 const val episodeVIII = "The Last Jedi"
 const val episodeIX = "Episode IX"
+
+
+fun <T> printWithLabel(label: String, element: T?) {
+    println("$label $element")
+}
+
+sealed class Quote: Throwable() {
+    class NeverSaidThat : Quote()
+}
+
+const val itsNotMyFault = "It’s not my fault."
+const val doOrDoNot = "Do. Or do not. There is no try."
+const val lackOfFaith = "I find your lack of faith disturbing."
+const val eyesCanDeceive = "Your eyes can deceive you. Don’t trust them."
+const val stayOnTarget = "Stay on target."
+const val iAmYourFather = "Luke, I am your father"
+const val useTheForce = "Use the Force, Luke."
+const val theForceIsStrong = "The Force is strong with this one."
+const val mayTheForceBeWithYou = "May the Force be with you."
+const val mayThe4thBeWithYou = "May the 4th be with you."
---
+++ src/main/kotlin/starting/eight.kt
@@ -0,0 +1,51 @@
+package starting
+
+import doOrDoNot
+import exampleOf
+import eyesCanDeceive
+import io.reactivex.rxkotlin.subscribeBy
+import io.reactivex.subjects.PublishSubject
+import itsNotMyFault
+import lackOfFaith
+import printWithLabel
+import stayOnTarget
+
+fun main(args: Array<String>) {
+    exampleOf("PublishSubject") {
+
+        val quotes = PublishSubject.create<String>()
+
+        quotes.onNext(itsNotMyFault)
+
+        val subscriptionOne = quotes.subscribeBy(
+                onNext = { printWithLabel("1)", it)},
+                onComplete = { printWithLabel("1)", "Complete")}
+        )
+
+        quotes.onNext(doOrDoNot)
+
+        val subscriptionTwo = quotes.subscribeBy(
+                onNext = { printWithLabel("2)", it)},
+                onComplete = { printWithLabel("2)", "Complete")}
+        )
+
+        quotes.onNext(lackOfFaith)
+
+        subscriptionOne.dispose()
+
+        quotes.onNext(eyesCanDeceive)
+
+        quotes.onComplete()
+
+        val subscriptionThree = quotes.subscribeBy(
+                onNext = { printWithLabel("3)", it)},
+                onComplete = { printWithLabel("3)", "Complete")}
+        )
+
+        quotes.onNext(stayOnTarget)
+
+        subscriptionTwo.dispose()
+        subscriptionThree.dispose()
+
+    }
+}
