5c21989c82ed9e6b6015abbfad08153604f44203
Aung Thiha <mr.aungthiha@gmail.com>
2019-04-08 22:33:32 +0700

    the use of side effects like doOnError

--- src/main/kotlin/starting/seven.kt
+++ src/main/kotlin/starting/seven.kt
@@ -14,33 +14,6 @@ import rogueOne
 
 fun main(args: Array<String>) {
 
-    exampleOf("subscribe") {
-
-        val observable = Observable.just(episodeIV, episodeV, episodeVI)
-
-        observable.subscribeBy(onNext = {
-            println(it)
-        }, onComplete = {
-            println("Completed")
-        })
-
-        observable.subscribe { element ->
-            println(element)
-        }
-    }
-
-    exampleOf("empty") {
-
-        val observable = Observable.empty<Unit>()
-
-        observable.subscribeBy(onNext = {
-            println(it)
-        }, onComplete = {
-            println("Completed")
-        })
-
-    }
-
     exampleOf("never") {
 
         val subscriptions = CompositeDisposable()
@@ -59,30 +32,6 @@ fun main(args: Array<String>) {
         })
 
         subscriptions.add(subscription)
-    }
-
-    exampleOf("dispose") {
-
-        val mostPopular: Observable<String> = Observable.just(episodeV, episodeIV, episodeVI)
-
-        val subscription = mostPopular.subscribe {
-            println(it)
-        }
-
-        subscription.dispose()
-    }
-
-    exampleOf("CompositeDisposable") {
-
-        val subscriptions = CompositeDisposable()
-
-        subscriptions.add(listOf(episodeVII, episodeI, rogueOne)
-                .toObservable()
-                .subscribeBy {
-                    println(it)
-                })
-
-        subscriptions.dispose()
     }
 
 }
