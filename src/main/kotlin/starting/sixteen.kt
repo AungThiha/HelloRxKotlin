81b4cc83f353624f8ec05568f616497308726bd6
Aung Thiha <mr.aungthiha@gmail.com>
2019-04-20 22:36:21 +0700

    Observable.filter

--- src/main/kotlin/starting/sixteen.kt
+++ src/main/kotlin/starting/sixteen.kt
@@ -2,6 +2,7 @@ package starting
 
 import detours
 import exampleOf
+import io.reactivex.Observable
 import io.reactivex.disposables.CompositeDisposable
 import io.reactivex.rxkotlin.addTo
 import io.reactivex.rxkotlin.subscribeBy
@@ -10,6 +11,7 @@ import landOfDroids
 import liveLongAndProsper
 import mayTheForce
 import mayTheOdds
+import tomatometerRatings
 import wookieWorld
 
 fun main(args: Array<String>) {
@@ -52,6 +54,19 @@ fun main(args: Array<String>) {
 
         quotes.onNext(mayTheForce)
 
+    }
+
+    exampleOf("Filter") {
+
+        val subscriptions = CompositeDisposable()
+
+        Observable.fromIterable(tomatometerRatings)
+                .filter {
+                    it.rating >= 90
+                }
+                .subscribeBy {
+                    println(it)
+                }.addTo(subscriptions)
     }
 
 }
